8ae4a3f64747d2780a82a01a203e2e4bff865a39
Rajan <rajafaslur@gmail.com>
2020-08-21 18:30:11 +0530

    initial Release

---
+++ example/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plyr/flutter_plyr.dart';
+
+
+void main() {
+  runApp(PlyrVideoPlayer());
+}
+
+class PlyrVideoPlayer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            child: Plyr('AGBjI0x9VbM')),
+      ),
+    );
+  }
+}
---
+++ lib/flutter_plyr.dart
@@ -0,0 +1,55 @@
+library flutter_plyr;
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+
+const kAndroidUserAgent =
+    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
+
+String selectedUrl = 'https://flutter.io';
+
+// ignore: prefer_collection_literals
+final Set<JavascriptChannel> jsChannels = [
+  JavascriptChannel(
+      name: 'Print',
+      onMessageReceived: (JavascriptMessage message) {
+        print(message.message);
+      }),
+].toSet();
+
+
+
+// ignore: must_be_immutable
+class Plyr extends StatelessWidget {
+  Map data;
+  String videoID;
+  Plyr(this.videoID);
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Plyr',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WebviewScaffold(
+        url: 'https://royaliosvideoplayer.web.app/$videoID',
+        javascriptChannels: jsChannels,
+        mediaPlaybackRequiresUserGesture: false,
+        withZoom: true,
+        withLocalStorage: true,
+        hidden: true,
+        initialChild: Container(
+          color: Colors.white,
+          child: const Center(
+            child: CircularProgressIndicator()
+          ),
+        ),
+
+      ),
+    );
+  }
+}
